9563bb42dd3bcd2317424bd5b288280e24ae7462
Aaron Oppenheimer <aoppenheimer@daktaridx.com>
2013-08-26 23:16:37 -0400

    started work on integrating plans

--- agenda.py
+++ agenda.py
@@ -1,12 +1,14 @@
 from google.appengine.api import users
 
 import webapp2
-from member import *
-from assoc import *
-from gig import *
+import member
+import assoc
+import gig
+import plan
+import band
 
 from jinja2env import jinja_environment as je
-import debug
+from debug import debug_print
     
 class MainPage(webapp2.RequestHandler):
 
@@ -20,36 +22,45 @@ class MainPage(webapp2.RequestHandler):
     def make_page(self,user):
         debug_print('IN AGENDA {0}'.format(user.nickname()))
         
-        member=get_member_from_nickname(user.nickname())
+        the_member=member.get_member_from_nickname(user.nickname())
         debug_print('member is {0}'.format(str(member)))
         
-        if member is None:
+        if the_member is None:
             return # todo figure out what to do if we get this far and there's no member
             
         # find the bands this member is associated with
-        bands=get_bands_of_member(member)
+        the_bands=member.get_bands_of_member(the_member)
         
-        if bands is None:
+        if the_bands is None:
             return # todo figure out what to do if there are no bands for this member
             
-        if len(bands) > 1:
+        if len(the_bands) > 1:
             return # todo figure out what to do if there is more than one band for this member
         
-        band=bands[0]
+        the_band=the_bands[0]
         
-        gigs=get_gigs_for_band(band, num=2)
+        the_gigs=gig.get_gigs_for_band(the_band, num=2)
         
         gig_info=[]
-        for gig in gigs:
-            gig_info.append( [gig.title, gig.key.id()] )
+        for a_gig in the_gigs:
+            the_plan=plan.get_plan_for_member_for_gig(the_member, a_gig)
+            gig_info.append( [a_gig, a_gig.key.id(), the_plan] )
         
+        all_gigs=gig.get_gigs_for_band(the_band)
+        weigh_ins=[]
+        for a_gig in all_gigs:
+            the_plan=plan.get_plan_for_member_for_gig(the_member, a_gig)
+            if the_plan is None:
+                weigh_ins.append( [a_gig.title, a_gig.key.id()] )
+
         template = je.get_template('agenda.html')
         self.response.write( template.render(
             title='Agenda',
-            member=member,
+            member=the_member,
             logout_link=users.create_logout_url('/'),
-            band=band,
-            band_id=band.key.id(),
+            band=the_band,
+            band_id=the_band.key.id(),
             gigs=gig_info,
+            weigh_ins=weigh_ins,
             agenda_is_active=True
         ) )        
--- plan.py
+++ plan.py
@@ -6,8 +6,7 @@
 #
 
 from google.appengine.ext import ndb
-from member import *
-from debug import *
+from debug import debug_print
 
 #
 # class for plan
@@ -17,15 +16,26 @@ class Plan(ndb.Model):
     member = ndb.KeyProperty()
     value = ndb.IntegerProperty()
 
-def new_plan(gig, member, value):
+def new_plan(the_gig, the_member, value):
     """ associate a gig and a member """
-    the_plan = Plan(parent=gig.key, member=member.key, value=value)
+    the_plan = Plan(parent=the_gig.key, the_member=member.key, value=value)
     the_plan.put()
 
-def get_plans_for_gig(gig):
+def get_plans_for_gig(the_gig):
     """ Return plan objects by gig"""
-    plan_query = Plan.query(ancestor=gig.key)
+    plan_query = Plan.query(ancestor=the_gig.key)
     plans = plan_query.fetch()
-    debug_print('get_plans_for_gig: got {0} plans for gig key id {1} ({2})'.format(len(plans),gig.key.id(),gig.title))
+    debug_print('get_plans_for_gig: got {0} plans for gig key id {1} ({2})'.format(len(plans),the_gig.key.id(),the_gig.title))
     return plans
 
+def get_plan_for_member_for_gig(the_member, the_gig):
+    plan_query = Plan.query(Plan.member==the_member.key, ancestor=the_gig.key)
+    plans = plan_query.fetch()
+    debug_print('get_plans_for_gig: got {0} plans for gig key id {1} ({2})'.format(len(plans),the_gig.key.id(),the_gig.title))
+    if len(plans)>1:
+        return None #todo what to do if there's more than one plan
+        
+    if len(plans)>0:
+        return plans[0]
+    else:
+        return None
