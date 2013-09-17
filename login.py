ac7017665a4abacf0e15c94e69c1973be2ddfc5f
Aaron Oppenheimer <aoppenheimer@daktaridx.com>
2013-09-17 15:51:02 +0200

    added forgot password methods

--- login.py
+++ login.py
@@ -125,12 +125,78 @@ class VerificationHandler(BaseHandler):
         else:
             logging.info('verification type not supported')
             self.abort(404)
-            
-            
+
+##########
+#
+# SetPasswordHandler
+#
+##########
+class SetPasswordHandler(BaseHandler):
+
+  @user_required
+  def post(self):
+    password = self.request.get('password')
+    old_token = self.request.get('t')
+
+    if not password or password != self.request.get('confirm_password'):
+      self.display_message('passwords do not match')
+      return
+
+    user = self.user
+    user.set_password(password)
+    user.put()
+
+    # remove signup token, we don't want users to come back with an old link
+    self.user_model.delete_signup_token(user.get_id(), old_token)
+    
+    self.display_message('Password updated')
+
+##########
+#
+# AuthenticatedHandler
+#
+##########
 class AuthenticatedHandler(BaseHandler):
     @user_required
     def get(self):
         self.render_template('authenticated.html')
+
+##########
+#
+# ForgotPasswordHandler
+#
+##########
+class ForgotPasswordHandler(BaseHandler):
+  def get(self):
+    self._serve_page()
+
+  def post(self):
+    username = self.request.get('username')
+
+    user = self.user_model.get_by_auth_id(username)
+    if not user:
+      logging.info('Could not find any user entry for username %s', username)
+      self._serve_page(not_found=True)
+      return
+
+    user_id = user.get_id()
+    token = self.user_model.create_signup_token(user_id)
+
+    verification_url = self.uri_for('verification', type='p', user_id=user_id,
+      signup_token=token, _full=True)
+
+    msg = 'Send an email to user in order to reset their password. \
+          They will be able to do so by visiting <a href="{url}">{url}</a>'
+
+    self.display_message(msg.format(url=verification_url))
+  
+  def _serve_page(self, not_found=False):
+    username = self.request.get('username')
+    params = {
+      'username': username,
+      'not_found': not_found
+    }
+    self.render_template('forgot.html', params)
 
 config = {
     'webapp2_extras.auth': {
