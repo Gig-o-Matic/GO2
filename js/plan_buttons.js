f72d258d6dfd6ac606a22f2bd1c6dbed21971c39
Aaron Oppenheimer <aoppenheimer@daktaridx.com>
2013-09-18 23:45:23 +0200

    fixed up plan editing so there's only one element. added back the comment field.

--- js/plan_buttons.js
+++ js/plan_buttons.js
@@ -44,6 +44,22 @@ function update_plan(pk, val) {
                 });
 }
 
+function update_comment(elem, pk) {
+        var val=elem.value;
+        $.post("/updateplancomment",
+                    {
+                        val: val,
+                        pk: pk
+                    },
+                    function(responseTxt,statusTxt,xhr){
+                        if(statusTxt=="success")
+                            elem.parentNode.className="form-group has-success";
+                            setTimeout(function(){elem.parentNode.className="form-group";}, 1000);
+                        if(statusTxt=="error")
+                          alert("Error: "+xhr.status+": "+xhr.statusText);
+                    });
+
+}
 
 $(document).ready(function() {
     init_plan_buttons();
