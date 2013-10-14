15bd9e5150266a868b5d5522951c8d97e7f3014d
Aaron Oppenheimer <aoppenheimer@daktaridx.com>
2013-10-14 19:03:04 -0400

    added credits page

---
+++ credits.py
@@ -0,0 +1,21 @@
+from google.appengine.api import users
+
+from requestmodel import *
+
+import webapp2
+from jinja2env import jinja_environment as je
+    
+class CreditsHandler(BaseHandler):
+
+    @user_required
+    def get(self):    
+        """ get handler for credits page """
+        self._make_page(the_user=self.user)
+            
+    def _make_page(self,the_user):
+        """ construct page for help """
+
+        template_args = {
+            'title' : 'Credits',
+        }
+        self.render_template('credits.html', template_args)
