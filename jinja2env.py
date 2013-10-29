c06010af205af13077dc7ae24705dd0c18b72afe
Aaron Oppenheimer <aoppenheimer@daktaridx.com>
2013-10-28 20:15:57 -0400

    turned on autoescape

--- jinja2env.py
+++ jinja2env.py
@@ -1,7 +1,19 @@
 import jinja2
 import os
 
-jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)+"/templates"),
-    extensions=['jinja2.ext.autoescape'])
+
+def guess_autoescape(template_name):
+    if template_name is None or '.' not in template_name:
+        return False
+    ext = template_name.rsplit('.', 1)[1]
+    return ext in ('html', 'htm', 'xml')
+
+jinja_environment = jinja2.Environment(autoescape=guess_autoescape,
+                  loader=jinja2.FileSystemLoader(os.path.dirname(__file__)+"/templates"),
+                  extensions=['jinja2.ext.autoescape'])
+
+
+# jinja_environment = jinja2.Environment(
+#     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)+"/templates"),
+#     extensions=['jinja2.ext.autoescape'])
 
