1d2ab0380b775026689f632a18489a2a35060dd0
Aaron Oppenheimer <aoppenheimer@daktaridx.com>
2013-11-06 16:00:43 -0500

    encapsulates comment code

---
+++ js/comments.js
@@ -0,0 +1,31 @@
+function update_comment(gk) {
+    $.post("/gig_get_comment",
+                {
+                    gk: gk,
+                },
+                function(responseTxt,statusTxt,xhr){
+                    if(statusTxt=="success")
+                        $('#gig_comment').html(responseTxt)
+                    if(statusTxt=="error")
+                        alert("Error: "+xhr.status+": "+xhr.statusText);
+                });
+}
+
+function add_comment(gk) {
+    var d = new Date();
+    offset=d.getTimezoneOffset()/60;
+    $.post("/gig_add_comment",
+                {
+                    gk: gk,
+                    c: $('#commentinput').val(),
+                    o: offset
+                },
+                function(responseTxt,statusTxt,xhr){
+                    if(statusTxt=="success")
+                        $('#commentinput').val('')
+                        update_comment(gk)
+                    if(statusTxt=="error")
+                        alert("Error: "+xhr.status+": "+xhr.statusText);
+                });
+    
+}
