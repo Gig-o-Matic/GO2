d4549847a38136e42d305010232ba9bd1d9ce5ab
aaronoppenheimer <aoppenheimer@gmail.com>
2017-04-15 13:03:42 -0400

    working on rss

--- rss.py
+++ rss.py
@@ -17,6 +17,7 @@ from google.appengine.api import app_identity
 
 import gig
 import band
+import member
 
 import datetime
 from pytz.gae import pytz
@@ -25,24 +26,31 @@ import pickle
 def make_gig_feed(the_band):
 
     the_gigs = gig.get_gigs_for_band_keys(the_band.key, show_canceled=False, show_only_public=True, show_past=False, start_date=datetime.datetime.now())
-    feed = """
-<?xml version="1.0" encoding="UTF-8" ?>
+    feed = u"""<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0">
-
 <channel>
 """
 
-    feed="{0}\n<title>{1} Gigs</title>".format(feed, the_band.name)
-    feed="{0}\n<link>{1}{2}</link>".format(feed, "gig-o-matic.appspot.com/feeds/",the_band.key.urlsafe())
-    feed="{0}\n<description>{1} Gigs</title>".format(feed, the_band.name)
+    feed=u"{0}\n<title>{1} Gigs</title>".format(feed, the_band.name)
+    feed=u"{0}\n<link>{1}{2}</link>".format(feed, "http://gig-o-matic.appspot.com/feeds/",the_band.key.urlsafe())
+    feed=u"{0}\n<description>{1} Gigs</description>".format(feed, the_band.name)
 
     for a_gig in the_gigs:
-        feed="{0}\n<item>".format(feed)
-        feed="{0}\n<title>{1}</title>".format(feed, a_gig.title)
-        feed="{0}\n<guid>{1}</guid>".format(feed, a_gig.key.urlsafe())
-        feed="{0}\n<description>{1}</description>".format(feed, a_gig.rss_description)
-        feed="{0}\n</item>".format(feed)
-    feed="{0}{1}".format(feed,"""
+        feed=u"{0}\n<item>".format(feed)
+        feed=u"{0}\n<title>{1}</title>".format(feed, a_gig.title)
+        feed=u'{0}\n<guid isPermaLink="false">{1}</guid>'.format(feed, a_gig.key.urlsafe())
+        if a_gig.contact:
+            the_date = member.format_date_for_member(a_gig.contact.get(),a_gig.date,"long")
+        else:
+            the_date = a_gig.date
+
+        if a_gig.settime:
+            the_time = u' - {0}'.format(a_gig.settime)
+        else:
+            the_time = u''
+        feed=u"{0}\n<description>{1}{2}\n\n{3}</description>".format(feed, the_date, the_time, a_gig.rss_description)
+        feed=u"{0}\n</item>".format(feed)
+    feed=u"{0}{1}".format(feed,"""
 </channel>
 </rss>
 """)
@@ -68,8 +76,9 @@ def store_feed_for_band_key(the_band_key, the_feed):
                       'w',
                       content_type='text/plain',
                       retry_params=write_retry_params)
-    gcs_file.write(the_feed)
+    gcs_file.write(the_feed.encode('UTF-8'))
     gcs_file.close()
+    return the_feed
 
 def get_feed_for_band_key(the_band_key):
     bucket_name = os.environ.get('BUCKET_NAME',
@@ -88,13 +97,11 @@ def get_feed_for_band_key(the_band_key):
         feed = gcs_file.read()
         gcs_file.close()
     else:
-        feed = None
+        feed = ""
 
     return feed
 
 def make_rss_feed_for_band(the_band):
-    print('\n\nqueueing!\n\n')
-
     the_params = pickle.dumps({'the_band_key': the_band.key})
 
     task = taskqueue.add(
@@ -105,7 +112,6 @@ def make_rss_feed_for_band(the_band):
 class MakeRssFeedHandler(webapp2.RequestHandler):
 
     def post(self):
-        print('\n\nMADE FEED\n\n')
         the_params = pickle.loads(self.request.get('the_params'))
         the_band_key = the_params['the_band_key']
         the_band = the_band_key.get()
@@ -132,6 +138,7 @@ class GetRssHandler(BaseHandler):
 
         feed = get_feed_for_band_key(the_band.key)
 
+        self.response.content_type = 'application/rss+xml'
         self.response.write('{0}\n'.format(feed))
 
 
