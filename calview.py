d497d2e02a9c6e5cf5fc2e2cd9301b8fc8a6fa97
aaronoppenheimer <aoppenheimer@gmail.com>
2018-04-23 21:25:00 -0400

    cleaned up ugly, ugly code

--- calview.py
+++ calview.py
@@ -48,7 +48,7 @@ class CalEvents(BaseHandler):
         
         the_assocs = assoc.get_confirmed_assocs_of_member(the_member)
         the_band_keys = [a.band for a in the_assocs]
-        the_bands = ndb.get_multi(the_band_keys)
+        # the_bands = ndb.get_multi(the_band_keys)
 
         cindices={}
         for a in the_assocs:
@@ -56,24 +56,22 @@ class CalEvents(BaseHandler):
 
         gigs = []
 
-        for a_band in the_bands:
-            a_band_name = a_band.shortname if a_band.shortname else a_band.name
-            all_gigs = gig.get_gigs_for_band_keys(a_band.key, show_past=True)
-            for a_gig in all_gigs:
-                if not a_gig.is_canceled and not a_gig.hide_from_calendar:
-                    the_plan = plan.get_plan_for_member_key_for_gig_key(the_member_key, a_gig.key)
-                    if the_plan:
-                        # check member preferences
-                        # include gig if member wants to see all, or if gig is confirmed
-                        if a_gig.is_confirmed or the_member.preferences.calendar_show_only_confirmed == False:
-                            # incude gig if member wants to see all, or if has registered as maybe or definitely:
-                            if (the_plan.value > 0 and the_plan.value <= 3) or \
-                                (the_member.preferences.calendar_show_only_committed == False):
-                                    gigs.append(a_gig)
+        all_gigs = gig.get_gigs_for_band_keys(the_band_keys, show_past=True, start_date=start_date, end_date=end_date)
+        for a_gig in all_gigs:
+            if not a_gig.is_canceled and not a_gig.hide_from_calendar:
+                the_plan = plan.get_plan_for_member_key_for_gig_key(the_member_key, a_gig.key)
+                if the_plan:
+                    # check member preferences
+                    # include gig if member wants to see all, or if gig is confirmed
+                    if a_gig.is_confirmed or the_member.preferences.calendar_show_only_confirmed == False:
+                        # incude gig if member wants to see all, or if has registered as maybe or definitely:
+                        if (the_plan.value > 0 and the_plan.value <= 3) or \
+                            (the_member.preferences.calendar_show_only_committed == False):
+                                gigs.append(a_gig)
         
         events=[]
-        the_band_keys = [b.key for b in the_bands]
         num_bands = len(the_band_keys)
+        band_names={}
         
         for a_gig in gigs:
             band_key=a_gig.key.parent()
@@ -83,11 +81,17 @@ class CalEvents(BaseHandler):
 
             the_title = a_gig.title
             if num_bands > 1:
-                the_band = band_key.get()
-                if the_band.shortname:
-                    the_title = u'{0}:\n{1}'.format(the_band.shortname, the_title)
+                if band_key in band_names.keys():
+                    the_band_name = band_names[band_key]
                 else:
-                    the_title = u'{0}:\n{1}'.format(the_band.name, the_title)
+                    the_band = band_key.get()
+                    if the_band.shortname:
+                        the_band_name = the_band.shortname
+                    else:
+                        the_band_name = the_band.name
+                    band_names[band_key] = the_band_name
+
+                    the_title = u'{0}:\n{1}'.format(the_band_name, the_title)
             
 
             events.append({
