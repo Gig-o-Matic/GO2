6858ff47b6c93c91903d26228a29f8eddb4d1a1f
Benoit <b.rosa@unistra.fr>
2018-11-13 16:14:13 +0100

    small changes for french translation

--- lang.py
+++ lang.py
@@ -4,4 +4,4 @@
 # ao 1 feb 14
 #
 
-LOCALES = [ ['de','Deutsch'], ['en','English'], ['en_GB','English (UK)'], ['it','Italiano'] ]
+LOCALES = [ ['de','Deutsch'], ['en','English'], ['en_GB','English (UK)'], ['it','Italiano'], ['fr','Francais'] ]
