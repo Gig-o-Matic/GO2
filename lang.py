a1e9dc6ff04acf6b3dd94a5be1cb908684f6ed5e
aaronoppenheimer <aoppenheimer@gmail.com>
2019-06-11 19:41:44 -0400

    added french accent to francais

--- lang.py
+++ lang.py
@@ -4,4 +4,4 @@
 # ao 1 feb 14
 #
 
-LOCALES = [ ['de','Deutsch'], ['en','English'], ['en_GB','English (UK)'], ['it','Italiano'], ['fr','Francais'] ]
+LOCALES = [ ['de','Deutsch'], ['en','English'], ['en_GB','English (UK)'], ['it','Italiano'], ['fr',u'Fran\u00e7ais'] ]
