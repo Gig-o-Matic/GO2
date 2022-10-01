b85fd84dae84aa88ccc0198e5f860f05d8f8badf
Aaron Oppenheimer <aaron.oppenheimer@cfa.harvard.edu>
2022-10-01 12:02:50 -0400

    initial work on spanish translation

--- lang.py
+++ lang.py
@@ -4,4 +4,4 @@
 # ao 1 feb 14
 #
 
-LOCALES = [ ['de','Deutsch'], ['en','English'], ['en_GB','English (UK)'], ['it','Italiano'], ['fr',u'Fran\u00e7ais'] ]
+LOCALES = [ ['de','Deutsch'], ['en','English'], ['en_GB','English (UK)'], ['it','Italiano'], ['fr',u'Fran\u00e7ais'], ['es',u'Espa\u00f1ol'] ]
